4f9a6d68db687cec1499d52843c1db8352b20eac
abdelmadjidbettache <b.madjidprof@hotmail.fr>
2023-09-29 19:25:28 -0400

    first commit

---
+++ ComboBox/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComboBox
+{
+    internal class Client
+    {
+        string id;
+        string nom;
+        string prenom;
+
+        public string Id { get => id; set => id = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+
+        public Client(string id, string nom, string prenom)
+        {
+            this.id = id;
+            this.nom = nom;
+            this.prenom = prenom;
+        }
+    }
+}
---
+++ ComboBox/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ComboBox
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Client> clients;
+        public MainWindow()
+        {
+            InitializeComponent();
+            clients = new List<Client>();
+            clients.Add(new Client("123","Bob","Max"));
+            clients.Add(new Client("456", "Alain", "Depuis"));
+            clients.Add(new Client("789", "Abed", "Ali"));
+            cbo.DataContext = clients;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            label.Content = ((Client)cbo.SelectedItem).Prenom;
+        }
+    }
+}
